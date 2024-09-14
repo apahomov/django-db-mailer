fff3c13233764a24848eb4aed13d1db27c00dee8
Samoilenko Lev <SamoilenkoLev@gmail.com>
2024-09-14 07:13:51 +0700

    fix

--- dbmail/apps.py
+++ dbmail/apps.py
@@ -1,12 +1,11 @@
 # -*- encoding: utf-8 -*-
 
-from django.utils.translation import gettext as _
 from django.apps import AppConfig
 
 
 class DBMailConfig(AppConfig):
     name = 'dbmail'
-    verbose_name = _('DB Mailer')
+    verbose_name = 'DB Mailer'
 
     def ready(self):
         from dbmail import initial_signals
